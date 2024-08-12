7e9040527f136c31c94b9963b14bec7a8fda9d42
Cristy Almonte <44328476+cristyalmonte@users.noreply.github.com>
2024-08-11 22:53:15 -0400

    site for relay wizard

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "cinzel-var": ["Cinzel Variable", "serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+

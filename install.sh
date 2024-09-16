4e0c8a5c87b0f6e002ceedc588610349897fb770
J the Code Monkey <jaythecodemonkey.7vk7i@slmail.me>
2024-09-16 19:54:15 -0400

    chore: update relaywiz to rwz

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# URL to download the relaywiz binary
-RELAYWIZ_URL="https://github.com/nodetec/relaywizard/releases/download/v0.2.0/rwz"
+# URL to download the rwz binary
+RWZ_URL="https://github.com/nodetec/relaywizard/releases/download/v0.2.0/rwz"
 
 # Destination path for the downloaded binary
 DEST_PATH="/usr/local/bin/rwz"
 
-# Download the relaywiz binary
-echo "Downloading Relay Wizard from $RELAYWIZ_URL..."
-curl -L -o "$DEST_PATH" "$RELAYWIZ_URL"
+# Download the rwz binary
+echo "Downloading Relay Wizard from $RWZ_URL..."
+curl -L -o "$DEST_PATH" "$RWZ_URL"
 
 # Make the binary executable
 echo "Making rwz executable..."
 chmod +x "$DEST_PATH"
 
-# Run the relaywiz install command
+# Run the rwz install command
 echo "Running rwz install..."
 "$DEST_PATH" install < /dev/tty
 

1aec17f3de4385e0464359cca74ffb7a82bccea2
J the Code Monkey <jaythecodemonkey.7vk7i@slmail.me>
2024-09-16 23:41:35 -0400

    fix: update paths

--- install.sh
+++ install.sh
@@ -7,24 +7,26 @@ RWZ_TAR_GZ="rwz-0.2.0-x86_64-linux-gnu.tar.gz"
 RWZ_URL="https://github.com/nodetec/relaywizard/releases/download/v0.2.0/$RWZ_TAR_GZ"
 
 # Destination path for the rwz compressed binary
-TEMP_DIR="/tmp"
+TMP_RWZ_TAR_GZ="/tmp/$RWZ_TAR_GZ"
 
 # Download the rwz compressed binary
 echo "Downloading Relay Wizard from $RWZ_URL..."
-curl -L -o "$TEMP_DIR" "$RWZ_URL"
+curl -L -o "$TMP_RWZ_TAR_GZ" "$RWZ_URL"
 
 # Destination path for the downloaded binary
 DEST_PATH="/usr/local/bin"
 
 # Extract rwz binary to the binary destination path
 echo "Extracting Relay Wizard to $DEST_PATH..."
-tar -xf "$TEMP_DIR/$RWZ_TAR_GZ" -C "$DEST_PATH"
+tar -xf "$TMP_RWZ_TAR_GZ" -C "$DEST_PATH"
+
+# rwz binary name
+RWZ="rwz"
 
 # Make the binary executable
 echo "Making rwz executable..."
-chmod +x "$DEST_PATH"
+chmod +x "$DEST_PATH/$RWZ"
 
 # Run the rwz install command
 echo "Running rwz install..."
-"$DEST_PATH" install < /dev/tty
-
+"$DEST_PATH/$RWZ" install < /dev/tty
